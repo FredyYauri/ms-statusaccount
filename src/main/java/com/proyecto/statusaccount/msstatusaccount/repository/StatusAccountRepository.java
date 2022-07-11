77dc102ccc14cea5928972a0ecba28e1e21de1d7
fyauriba <fyauriba@everis.com>
2022-07-11 02:51:47 -0500

    corrección merge

--- src/main/java/com/proyecto/statusaccount/msstatusaccount/repository/StatusAccountRepository.java
+++ src/main/java/com/proyecto/statusaccount/msstatusaccount/repository/StatusAccountRepository.java
@@ -1,5 +1,6 @@
 package com.proyecto.statusaccount.msstatusaccount.repository;
 
+
 import com.proyecto.statusaccount.msstatusaccount.entity.StatusAccount;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
